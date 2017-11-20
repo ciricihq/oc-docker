fd41c8c8a7062de14887d3e6fb32977869241769
Genar Trias Ortiz <genar@acs.li>
2017-11-20 10:00:19 +0100

    First steps to plugins registration, refs #1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var oc = require('oc');
 var configuration = require('./config').configuration;
+var registerPlugins = require('./config').registerPlugins;
 
 var registry = new oc.Registry(configuration);
+
+// Register plugins defined in config.js
+registerPlugins(registry)
 
 registry.start(function(err, app){
   if(err){
@@ -9,3 +13,4 @@ registry.start(function(err, app){
     process.exit(1);
   }
 });
+
